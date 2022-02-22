d31c52f664ab46e9dc944712c608acbc6f833ad6
Zivan Madjarev <zivanmadjarev@gmail.com>
2022-02-22 18:33:03 +0100

    Added functionality for Add New Todo button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import AddTodo from './AddTodo';
+import TodoCollection from './TodoCollection';
 
 const Header = () => {
-  const handleClick = () => {
-    console.log('clicked');
-  };
+  const [activeForm, setActiveForm] = useState({
+    active: false,
+    buttonText: 'Add New Todo',
+  });
 
   return (
     <header className="mt-32 mx-auto text-center flex flex-col justify-center items-center">
       <h1 className="text-3xl text-bold">Todo App</h1>
       <button
         className="w-32 max-h-12 text-white mt-1 rounded-md bg-[#81B622] hover:bg-[#ECF87F] hover:text-black transition-colors"
-        onClick={handleClick}
+        onClick={() =>
+          setActiveForm({
+            active: !activeForm.active,
+            buttonText: !activeForm.active ? 'Close popup' : 'Add New Todo',
+          })
+        }
       >
-        Add New Todo
+        {activeForm.buttonText}
       </button>
+
+      {activeForm.active && <AddTodo />}
+      {!activeForm.active && <TodoCollection />}
     </header>
   );
 };
